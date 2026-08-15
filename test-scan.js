8aa11747d7bc4e6f39ba02c9d85b3b9f0478bb9e
exampe <cool@yup.com>
2026-08-15 16:55:08 -0500

    fix
    Former-commit-id: f83bb884bfa0471d353086f106d94aca8e90d317

--- test-scan.js
+++
@@ -1,9 +0,0 @@
-const { getRedisClient } = require('./src/lib/redisCache');
-async function run() {
-  const redis = getRedisClient();
-  const keysStrInd = await redis.keys('finra:individual:*');
-  const keysStrFirm = await redis.keys('finra:firm:*');
-  console.log("ind:", keysStrInd.slice(0, 5));
-  console.log("firm:", keysStrFirm.slice(0, 5));
-}
-run().catch(console.error);
